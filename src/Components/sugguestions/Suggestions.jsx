346e776fb3147c1656c51239555c82c336801572
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2024-06-01 10:47:53 +0530

    Suggestion file updated

--- src/Components/sugguestions/Suggestions.jsx
+++ src/Components/sugguestions/Suggestions.jsx
@@ -6,12 +6,34 @@ function Suggestions() {
   return (
     <>
       <div className="suggestions">
-        <div className="suggestions_title">Suggestions for you.</div>
+        <div className="suggestion_username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>
+                <img src="https://source.unsplash.com/random/800x600" alt="" />
+              </Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">_dragon333_</span>
+              <span className="relation">The Dragon Fighter</span>
+            </div>
+          </div>
+          <button className="follow_button">Switch</button>
+        </div>
+        <div className="suggestions_title">
+          Suggestions for you.
+          <button className="see_button">See All</button>
+        </div>
         <div className="suggestions_usernames">
           <div className="suggestion_username">
             <div className="username_left">
               <span className="avatar">
-                <Avatar>R</Avatar>
+                <Avatar>
+                  <img
+                    src="https://source.unsplash.com/random/803x603"
+                    alt=""
+                  />
+                </Avatar>
               </span>
               <div className="username_info">
                 <span className="username">_jonny1234</span>
@@ -24,7 +46,12 @@ function Suggestions() {
           <div className="suggestion_username">
             <div className="username_left">
               <span className="avatar">
-                <Avatar>R</Avatar>
+                <Avatar>
+                  <img
+                    src="https://source.unsplash.com/random/804x604"
+                    alt=""
+                  />
+                </Avatar>
               </span>
               <div className="username_info">
                 <span className="username">_jonny1234</span>
@@ -37,7 +64,12 @@ function Suggestions() {
           <div className="suggestion_username">
             <div className="username_left">
               <span className="avatar">
-                <Avatar>R</Avatar>
+                <Avatar>
+                  <img
+                    src="https://source.unsplash.com/random/801x601"
+                    alt=""
+                  />
+                </Avatar>
               </span>
               <div className="username_info">
                 <span className="username">_jonny1234</span>
